15a67ea92fc1c107df0f24f847327131db860522
Jatin <jatin.xportsoft@gmail.com>
2024-08-29 16:02:21 +0530

    Next js

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+
+export default function About() {
+  return (
+    <div class="m-2 grid grid-cols-1 gap-3 sm:grid-cols-2">
+    <div class="min-h-[150px] rounded-md bg-orange-500 m-1"></div>
+    <div class="min-h-[150px] rounded-md bg-blue-500 m-1"></div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    
+
   ],
   theme: {
     extend: {
@@ -14,5 +17,9 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+
+  ],
+  theme: {},
 };
